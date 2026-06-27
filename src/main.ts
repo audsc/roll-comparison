3d75ea37b3be5789899832d8b6ddd599153dbb37
Audrey <audreyscopeland@gmail.com>
2026-06-27 17:34:52 -0400

    Add for cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.ANGULAR_APP_URL ?? 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      ...(process.env.ANGULAR_APP_URL ? [process.env.ANGULAR_APP_URL] : []),
+    ],
     credentials: true,
   });
 
